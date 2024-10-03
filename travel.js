c31ddd2e9ead323691e0335a352f6bfb52eb994d
Elle Barnaby <ebarnaby@srcuk.com>
2024-10-03 15:51:17 +0100

    searchbar

--- travel.js
+++ travel.js
@@ -1,7 +1,14 @@
-fetch("travel.json").then(res => {
-    if(res.ok);
-    return res.json();
+fetch("travel.json")
+    .then(res => {
+        if(res.ok) return res.json();
     })
     .then(data => console.log(data))
     .catch(error => console.log("Error"))
 /*if json is not there, error!*/
+
+function search() {
+console.log(search)
+}
+
+/*make button call function*/
+/*create child for searchbar*/
