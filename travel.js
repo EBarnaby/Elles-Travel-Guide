f84f2d65948e6574f5645e411c53f34d13ce33b7
Elle Barnaby <ebarnaby@srcuk.com>
2024-10-07 15:52:35 +0100

    input is data
    connected the json data to the user input

--- travel.js
+++ travel.js
@@ -1,29 +1,24 @@
-fetch("travel.json")
+function typing() {
+console.log(typing)
+} //knows you're typing
+
+function getData(data, homeInput){
+    console.log(data[homeInput.toLowerCase()]) 
+} //gets data and makes input lowercase to match the json
+
+function search(homeInput){
+    fetch("travel.json")
     .then(res => {
         if(res.ok) return res.json();
     })
-    .then(data => console.log(data))
+    .then(data => getData(data, homeInput)) //assigns func to data and input
     .catch(error => console.log("Something's wrong..."))
-/*gets json file, if file is not there, error!*/
-
-const keys = Object.keys("travel.json")
-/*gets the keys from json*/
-console.log(keys["countries"])
-
-function typing() {
-console.log(typing)
 }
-
-function search(homeInput){
-      console.log(homeInput);
-}
-/*when press search, links options*/
 
 function cancel(){
     console.log(cancel)
 }
 
-/*html button, get elementbyid list of options
-then link the options by returning the json*/
+/*link the options by returning the json*/
 
-/* {} is object [] is array*/
+/* create html of boxes for dataaaaaa*/
